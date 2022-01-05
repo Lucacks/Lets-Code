532c8109765506c66ae0caad5693e4f54a7eba18
Lucas Alves <alveslucaso094@gmail.com>
2022-01-04 22:56:06 -0300

    Ex. 16 and 17

--- exercicios_extras/lista01/exercicio16.js
+++ exercicios_extras/lista01/exercicio16.js
@@ -2,3 +2,18 @@
 A lanchonete Gostosura vende apenas um tipo de sanduíche, cujo recheio inclui duas fatias de queijo, uma fatia de presunto e uma rodela de hambúrguer. Sabendo que cada fatia de queijo ou presunto pesa 50 gramas, e que a rodela de hambúrguer pesa 100 gramas, faça um algoritmo em que o dono forneça a quantidade de sanduíches a fazer, e a máquina informe as quantidades (em quilos) de queijo, presunto e carne necessários para compra.
 */
 
+const prompt = require('prompt-sync')({sigint: true});
+
+const sanduiche = {
+    pesoQueijo: 50,
+    pesoPresunto: 50,
+    pesoHamburguer: 100
+}
+
+let qtdSanduiches = parseInt(prompt("Quantidade de sanduíches a fazer: "))
+
+console.log(`
+Valor total (em Kg) dos ingredientes a serem utilizados:
+- Queijo: ${(sanduiche.pesoQueijo * qtdSanduiches) * 0.001} Kg
+- Presunto: ${(sanduiche.pesoPresunto * qtdSanduiches) * 0.001} Kg
+- Hambúrguer: ${(sanduiche.pesoHamburguer * qtdSanduiches) * 0.001} Kg`) 
--- exercicios_extras/lista01/exercicio17.js
+++ exercicios_extras/lista01/exercicio17.js
@@ -1,3 +1,9 @@
 /* 
-
+Alguns países medem temperaturas em graus Celsius, e outros em graus Fahrenheit. Faça um algoritmo para ler uma temperatura Celsius e imprimi-Ia em Fahrenheit (pesquise como fazer este tipo de conversão).
 */
+
+const prompt = require('prompt-sync')({sigint: true});
+
+let temperatura = Number(parseFloat(prompt('Digite a temperatura em °C: ')));
+
+console.log(`Temperatura em Fahrenheit: ${(temperatura * 1.8 + 32).toFixed(1)}`);
