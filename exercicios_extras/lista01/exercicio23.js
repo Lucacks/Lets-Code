291641e3de4e3265cf427d7532dd3eeace9eb865
Lucas Alves <alveslucaso094@gmail.com>
2022-02-15 20:29:27 -0300

    Node module start

--- exercicios_extras/lista01/exercicio23.js
+++ exercicios_extras/lista01/exercicio23.js
@@ -1,3 +1,10 @@
 /* 
-
+Num dia de sol, você deseja medir a altura de um prédio, porém, a trena não é suficientemente longa. Assumindo que seja possível medir sua sombra e a do prédio no chão, e que você lembre da sua altura, faça um algoritmo para ler os dados necessários e calcular a altura do prédio.
 */
+
+const prompt = require('prompt-sync')({sigint: true});
+
+const anguloSol = parseInt(prompt('Ângulo da sombra: '))
+const tamanhoSompra = parseInt(prompt('Tamanho da sombra em metros: '))
+
+console.log('Altura do prédio: ${')
--- exercicios_extras/lista01/exercicio36.js
+++ exercicios_extras/lista01/exercicio36.js
@@ -1,3 +1,3 @@
 /* 
-
+Faça um algoritmo que receba o valor do salário mínimo e o valor do salário de um funcionário, calcule e mostre a quantidade de salários mínimos que ganha esse funcionário.
 */
