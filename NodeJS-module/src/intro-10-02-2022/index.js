7450086e2a639c81842964b58b074b4e90ccfcea
Lucas Alves <alveslucaso094@gmail.com>
2022-03-01 17:54:50 -0300

    Don't know anymore

--- NodeJS-module/src/intro-10-02-2022/index.js
+++
@@ -1,7 +0,0 @@
-const { maiorDeIdade, imprimeIdade } = require('./verificador-de-idade')
-
-const args = process.argv;
-const idade = 2;
-
-imprimeIdade(idade);
-console.log(maiorDeIdade(idade));
--- NodeJS-module/src/linter-15-02-2022/README.md
+++
@@ -1,5 +0,0 @@
-# Técnicas de Programação I
-# Turma 818 | Bora ser Tech
-
-### Exercise 2 - Linter
-Install ESLint and config with AirBnb rules.
